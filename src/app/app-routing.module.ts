1a7d2a738f9cac705692341c375f69cae4de8656
saurabh pardeshi <saurap895955@gmail.com>
2023-09-20 13:13:30 +0530

    added nav bar and one component for adding the emp details.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule,MatButtonModule,MatToolbarModule]
 })
 export class AppRoutingModule { }
